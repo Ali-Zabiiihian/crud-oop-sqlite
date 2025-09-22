c288acdabeaf64cceeaa77d03a7450ebc487909f
Ali Zabihian <zabihianali95@gmail.com>
2025-09-22 11:07:19 +0330

    .

--- database/database.py
+++ database/database.py
@@ -7,7 +7,10 @@ DB_PATH = "database.db"
 class UserRepository:
     def __init__(self, db_path):
         self.db_path = db_path
-    
+        with sqlite3.connect(self.db_path) as conn:
+            cursor = conn.cursor()
+            cursor.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, email TEXT)")
+
     # create (C)
     def create(self, user_data):
         with sqlite3.connect(self.db_path) as conn:
@@ -15,6 +18,7 @@ class UserRepository:
             cursor = conn.cursor()
             query = "INSERT INTO users (name, email) VALUES (?, ?)"
             cursor.execute(query, (user_data['name'], user_data['email']))
+            conn.commit()
 
             return cursor.lastrowid
 
@@ -35,6 +39,7 @@ class UserRepository:
             cursor = conn.cursor()
             query = "UPDATE users SET name = ?, email = ? WHERE id = ?"
             cursor.execute(query, (user_data['name'], user_data['email'], user_id))
+            conn.commit()
 
     # delete (D)
     def delete(self, user_id):
@@ -43,5 +48,6 @@ class UserRepository:
             cursor = conn.cursor()
             query = "DELETE FROM users WHERE id = ?"
             cursor.execute(query, (user_id,))
+            conn.commit()
 
 
