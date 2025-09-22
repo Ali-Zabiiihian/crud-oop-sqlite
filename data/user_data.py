eb9750c45226c789ebb8d849f113e3e6275b0fc2
Ali Zabihian <zabihianali95@gmail.com>
2025-09-22 10:24:06 +0330

    .

--- data/user_data.py
+++ data/user_data.py
@@ -1,10 +1,7 @@
-def get_user_data():
-    user_name = input("enter user's name: ")
-    user_email = input("enter user's email: ")
+def get_user_data(user_name, user_email):
     user_data = {'name': user_name, 'email': user_email}
     return user_data
 
-def get_user_id():
-    user_id = int(input("enter user's id: "))
+def get_user_id(user_id):   
     return user_id
 
--- main.py
+++ main.py
@@ -2,12 +2,33 @@ import tkinter as tk
 from data.user_data import get_user_data, get_user_id
 from data.database import user_repo, UserRepository
 
+
+def add_btn():
+    root = tk.Tk()
+    root.title("Add User")
+    root.geometry("300x300")
+    name_label = tk.Label(root, text="Name:")
+    name_label.pack(pady=5)
+    name_entry = tk.Entry(root)
+    name_entry.pack(pady=5)
+    email_label = tk.Label(root, text="Email:")
+    email_label.pack(pady=5)
+    email_entry = tk.Entry(root)
+    email_entry.pack(pady=5)
+    add_button = tk.Button(root, text="Add User", command=lambda: get_user_data(name_entry.get(), email_entry.get()))
+    add_button.pack(pady=20)
+    root.mainloop()
+
+
+
+
+
 def user_operation():
     root = tk.Tk()
     root.title("user")
     root.geometry("300x300")
 
-    add = tk.Button(root, text="Add a new user", width=20, bg="lightgreen")
+    add = tk.Button(root, text="Add a new user", width=20, bg="lightgreen", command=add_btn)
     add.pack(pady=20)
     delete = tk.Button(root, text="Delete a user", width=20, bg="red")
     delete.pack(pady=20)
